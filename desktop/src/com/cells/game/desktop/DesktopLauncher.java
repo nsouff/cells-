08f771ba5d2f42733d09c2b84c2e3ce07ac014c1
Lucas Ketels <lucasketels@Lucass-MacBook-Pro.home>
2021-02-16 15:46:08 +0100

    Create project

---
+++ desktop/src/com/cells/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cells.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cells.game.CellsSquare;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CellsSquare(), config);
+	}
+}
