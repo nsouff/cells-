dc786e6ef5cfd1f9ad67b81e21af4d43592a1cde
Lucas Ketels <lucas.ketels@gmail.com>
2021-02-22 10:17:58 +0100

    Create abstract class Block

---
+++ core/src/com/cells/game/Block.java
@@ -0,0 +1,15 @@
+public abstract class Block{
+
+    private int xCor;
+    private int yCor;
+
+    public int getXCor(){
+        return this.xCor;
+    }
+
+    public int getYCor(){
+        return this.yCor;
+    }
+
+
+}
